20982c095e8aeffceee2f1d61872ec44c4d06f6e
unknown <xasaymusayev01@gmail.com>
2023-04-23 23:23:32 +0400

    not completed

---
+++ CustomList/CustomList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class CustomList<T>
+    {
+        
+        private T[] array;
+        public CustomList()
+        {
+            array = new T[0];
+        }
+
+        public void Add(T nese)
+        {
+            Array.Resize(ref array, array.Length+1);
+            array[array.Length-1] = nese;   
+        }
+
+
+        public T Find(Predicate<T> func)
+        {
+            foreach (T item in array)
+            {
+                if (func(item))
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+        
+
+    }
+}
---
+++ CustomList/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using CustomList;
+
+CustomList<string> customList = new CustomList<string>();
+
+
+
+customList.Add("xasay");
+customList.Add("musa");
+
+foreach (var item in customList)
+{
+    Console.WriteLine(item);
+}
+
+customList.Find(x => x == "xasay");
+
+
